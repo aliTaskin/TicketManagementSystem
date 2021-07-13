ed79b321e4c805e6d753261a1a0fb6ba7c97732f
Ali Taskin <psnnn123456@hotmail.com>
2021-07-13 15:41:53 +0300

    Needed backup in order to check a functionality

--- TicketManagementSystem/Controllers/UserController.cs
+++ TicketManagementSystem/Controllers/UserController.cs
@@ -4,16 +4,39 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TicketManagementSystem.Models;
 using TicketManagementSystem.Models.Tables;
 
 namespace TicketManagementSystem.Controllers
 {
     public class UserController : Controller
     {
-        readonly UserManager<TicketManagementUser> _userManager;
+        readonly UserManager<TicketManagementUser> _userManager;// User manager will control the 
         public UserController(UserManager<TicketManagementUser> userManager)//inject dependency
         {
             _userManager = userManager;
+        }
+
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignIn(TicketManagementUserViewModel appUserViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                TicketManagementUser appUser = new TicketManagementUser
+                {
+                    UserName = appUserViewModel.UserName,
+                    Email = appUserViewModel.Email
+                };
+                IdentityResult result = await _userManager.CreateAsync(appUser, appUserViewModel.Sifre);
+                if (result.Succeeded)
+                    return RedirectToAction("Index");
+            }
+            return View();
         }
 
 
---
+++ TicketManagementSystem/Models/TicketManagementUserViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TicketManagementSystem.Models
+{
+    public class TicketManagementUserViewModel
+    {
+        [Required(ErrorMessage = "Please Enter User Name")]
+        [StringLength(15, ErrorMessage = "Please enter a valid user name", MinimumLength = 4)]
+        [Display(Name = "UserName")]
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "Please Enter E-Mail")]
+        [EmailAddress(ErrorMessage = "Please enter a valid email")]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Please Enter Password")]
+        [DataType(DataType.Password, ErrorMessage = "Please enter a valid password")]
+        [Display(Name = "Password")]
+        public string Sifre { get; set; }
+    }
+}
